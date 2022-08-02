cdac39ae53c4d0e38500834d8c2c3d81fa8556a8
천수민 <102133017+smthousand@users.noreply.github.com>
2022-08-02 11:56:33 +0900

    Ex. 22 Answer

--- 21_Our First Conditional Exercise/index.js
+++ 21_Our First Conditional Exercise/index.js
@@ -5,11 +5,11 @@
 
 function isEven(num){
     //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
-    if(num % 2 === 0) {
-        console.log("even")
+    if (num % 2 === 0) { // num을 2로 나눗셈 했을 때 나머지가 0과 같을 때 짝수
+        console.log("even");
     }
     
-//    if(num % 2 === 1) {
+//    if(num % 2 === 1) { // num을 2로 나눗셈 했을 때 나머지가 1과 같을 때 홀수
 //        console.log("odd")
 //    }
    
